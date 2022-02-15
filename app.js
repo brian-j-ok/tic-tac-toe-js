bdc6ae18e31cc833cdc3d88c62b3fd296e6e27c0
brianjrok <brianjrok@gmail.com>
2022-02-15 14:00:50 -0500

    set up event listner on grid

--- app.js
+++ app.js
@@ -14,11 +14,30 @@ const Player = (name) => {
   return {getName};
 };
 
-// Display Controlller Module
+// Display Controller Module
 const displayController = (() => {
-  const gameboard = document.getElementById('gameboard');
+  const grid_array = document.querySelectorAll(".grid");
+
+  grid_array.forEach((div) => {
+    div.addEventListener("click", function () {
+      if (this.innerHTML != '') return
+
+      this.innerHTML = "X";
+    });
+  });
 
   return {
-
+    
   };
-}) ();
+})();
+
+// Game Module
+const game = (() => {
+  const player1 = Player('Brian');
+  const player2 = Player('Meg');
+
+  return {player1, player2};
+})();
+
+console.log(game.player1.getName());
+console.log(game.player2.getName());
